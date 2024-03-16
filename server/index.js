8fd8022dd5c857d63d1e9af37abb1053cc764594
prajwal raut <90103440+Prajwalraut29@users.noreply.github.com>
2024-03-16 11:59:30 +0530

    fix : deployment

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ const cookieParser = require("cookie-parser");
 const { connection } = require("../server/db/connection");
 const router = require('../server/routes/routes')
 require("dotenv").config();
-const BASE_URL = process.env.BASE_URL
+const PORT = process.env.BASE_URL || 5000
 
 connection();
 
@@ -15,6 +15,6 @@ app.use(cookieParser());
 
 app.use("/api", router);
 
-app.listen(BASE_URL, () => console.log(`Server running on port ${process.env.port}`));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 
